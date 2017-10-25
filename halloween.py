2be750c089951aaf1e59cb800c22b92ba9deffd9
nick <nick@NICK-WIN10>
2017-10-25 11:18:48 -0700

    add connect code

--- halloween.py
+++ halloween.py
@@ -6,18 +6,22 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.IN)
 GPIO.setup(23, GPIO.IN)
 GPIO.setup(24, GPIO.IN)
 GPIO.setup(25, GPIO.IN)
 
 while True:
+	if (GPIO.input(22) == False):
+        os.system('echo "connect AA:BB:CC:DD:EE:FF\nquit" | bluetoothctl &')
+
     if (GPIO.input(23) == False):
-        os.system('mpg123 -q binary-language-moisture-evaporators.mp3 &')
+        os.system('mpg123 -q file1.mp3 &')
 
     if (GPIO.input(24) == False):
-        os.system('mpg123 -q power-converters.mp3 &')
+        os.system('mpg123 -q file2.mp3 &')
 
     if (GPIO.input(25)== False):
-        os.system('mpg123 -q vader.mp3 &')
+        os.system('mpg123 -q file3.mp3 &')
 
     sleep(0.1);
